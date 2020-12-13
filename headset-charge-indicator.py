f5b599f439cf3f5120ff77d62ed7a7919fd673d2
m0ellemeister <m0ellemeister@users.noreply.github.com>
2020-12-13 15:49:20 +0100

    some refactoring
    based on suggestions by PyCharm

--- headset-charge-indicator.py
+++ headset-charge-indicator.py
@@ -8,9 +8,11 @@
 # Simple start this application as background process, i.e. during
 # startup of the graphical desktop
 
+import argparse
 from sys import argv
 
 from gi import require_version
+
 require_version('Gtk', '3.0')
 require_version('AppIndicator3', '0.1')
 
@@ -39,10 +41,11 @@ chatmix = None
 global prevSwitch
 prevSwitch = 0
 
+
 def change_icon(dummy):
     global prevSwitch
     try:
-        output = check_output([SWITCHSOUND_BINARY,"-1"] )
+        output = check_output([SWITCHSOUND_BINARY, "-1"])
         # only 
         if prevSwitch == 0:
             # exit 0 means we could not find out, so set some other icon
@@ -64,35 +67,37 @@ def change_icon(dummy):
 
     return True
 
+
 def change_label(dummy):
     try:
-        output = check_output([HEADSETCONTROL_BINARY,OPTION_BATTERY,OPTION_SILENT] )
+        output = check_output([HEADSETCONTROL_BINARY, OPTION_BATTERY, OPTION_SILENT])
         print('Bat: ' + str(output, 'utf-8'))
         # -1 indicates "Battery is charging"
         if int(output) == -1:
             ind.set_label('Chg', '999%')
-            ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+            ind.set_status(appindicator.IndicatorStatus.ACTIVE)
         # -2 indicates "Battery is unavailable"
         elif int(output) == -2:
             ind.set_label('Off', '999%')
-            ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+            ind.set_status(appindicator.IndicatorStatus.ACTIVE)
         elif int(output) < 100:
             ind.set_label(str(output, 'utf-8') + '%', '999%')
-            ind.set_status (appindicator.IndicatorStatus.ATTENTION)
+            ind.set_status(appindicator.IndicatorStatus.ATTENTION)
         else:
             ind.set_label(str(output, 'utf-8') + '%', '999%')
-            ind.set_status (appindicator.IndicatorStatus.ACTIVE)
+            ind.set_status(appindicator.IndicatorStatus.ACTIVE)
     except CalledProcessError as e:
         print(e)
         ind.set_label('N/A', '999%')
 
     return True
 
+
 def change_chatmix(dummy):
     global chatmix
 
     try:
-        output = check_output([HEADSETCONTROL_BINARY,OPTION_CHATMIX,OPTION_SILENT] )
+        output = check_output([HEADSETCONTROL_BINARY, OPTION_CHATMIX, OPTION_SILENT])
         print("ChatMix: " + str(output, 'utf-8'))
         chatmix.get_child().set_text('ChatMix: ' + str(output, 'utf-8'))
     except CalledProcessError as e:
@@ -101,30 +106,33 @@ def change_chatmix(dummy):
 
     return True
 
+
 def set_sidetone(dummy, level):
     print("Set sidetone to: " + str(level))
     try:
-        output = check_output([HEADSETCONTROL_BINARY,OPTION_SIDETONE,str(level),OPTION_SILENT] )
+        output = check_output([HEADSETCONTROL_BINARY, OPTION_SIDETONE, str(level), OPTION_SILENT])
         print("Result: " + str(output, 'utf-8'))
     except CalledProcessError as e:
         print(e)
 
     return True
+
 
 def set_led(dummy, level):
     print("Set LED to: " + str(level))
     try:
-        output = check_output([HEADSETCONTROL_BINARY,OPTION_LED,str(level),OPTION_SILENT] )
+        output = check_output([HEADSETCONTROL_BINARY, OPTION_LED, str(level), OPTION_SILENT])
         print("Result: " + str(output, 'utf-8'))
     except CalledProcessError as e:
         print(e)
 
     return True
 
+
 def switch_sound(dummy, level):
     print("Switch sound to: " + str(level))
     try:
-        output = check_output([SWITCHSOUND_BINARY,str(level)] )
+        output = check_output([SWITCHSOUND_BINARY, str(level)])
         print("Result: " + str(output, 'utf-8'))
     except CalledProcessError as e:
         print(e)
@@ -134,14 +142,15 @@ def switch_sound(dummy, level):
 
     return True
 
+
 def sidetone_menu():
     # we map 5 levels to the range of [0-128]
     # The Steelseries Arctis internally supports 0-0x12, i.e. 0-18
-#    OFF -> 0
-#    LOW -> 32
-#    MEDIUM -> 64
-#    HIGH -> 96
-#    MAX -> 128
+    #    OFF -> 0
+    #    LOW -> 32
+    #    MEDIUM -> 64
+    #    HIGH -> 96
+    #    MAX -> 128
 
     sidemenu = Gtk.Menu()
 
@@ -172,6 +181,7 @@ def sidetone_menu():
 
     return sidemenu
 
+
 def led_menu():
     ledmenu = Gtk.Menu()
 
@@ -186,6 +196,7 @@ def led_menu():
     on.show_all()
 
     return ledmenu
+
 
 def switch_menu():
     switchmenu = Gtk.Menu()
@@ -212,76 +223,80 @@ def switch_menu():
 
     return switchmenu
 
+
 def refresh(dummy):
     change_label(None)
     change_chatmix(None)
     if len(argv) == 3:
         change_icon(None)
 
-def quit(source):
+
+def quit_app(source):
     Gtk.main_quit()
 
+
 if __name__ == "__main__":
-  if len(argv) != 2 and len(argv) != 3:
-    print("Need one or two commandline arguments, one for the location of the HeadsetControl binary and one for the optional command to switch between Laptop and Headset")
-    exit(1)
+    if len(argv) != 2 and len(argv) != 3:
+        print(
+            "Need one or two commandline arguments, one for the location of the HeadsetControl binary and one for the optional command to switch between Laptop and Headset")
+        exit(1)
 
-  HEADSETCONTROL_BINARY = argv[1]
-  if len(argv) == 3:
-    SWITCHSOUND_BINARY = argv[2]
+    HEADSETCONTROL_BINARY = argv[1]
+    if len(argv) == 3:
+        SWITCHSOUND_BINARY = argv[2]
 
-  ind = appindicator.Indicator.new (
-                        APPINDICATOR_ID,
-                        "audio-headset",
-                        appindicator.IndicatorCategory.HARDWARE )
-  ind.set_status (appindicator.IndicatorStatus.ACTIVE)
-  ind.set_label("-1%", '999%')
+    ind = appindicator.Indicator.new(
+        APPINDICATOR_ID,
+        "audio-headset",
+        appindicator.IndicatorCategory.HARDWARE)
+    ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+    ind.set_label("-1%", '999%')
 
-  # create a menu with an Exit-item
-  menu = Gtk.Menu()
-  
-  menu_items = Gtk.MenuItem(label="Refresh")
-  menu.append(menu_items)
-  menu_items.connect("activate", refresh)
-  menu_items.show_all()
-  
-  menu_items = Gtk.MenuItem(label="Chat: -1")
-  menu.append(menu_items)
-  menu_items.show_all()
-  chatmix = menu_items
+    # create a menu with an Exit-item
+    menu = Gtk.Menu()
 
-  menu_items = Gtk.MenuItem(label="Sidetone")
-  menu.append(menu_items)
-  menu_items.show_all()
-  menu_items.set_submenu(sidetone_menu())
+    menu_items = Gtk.MenuItem(label="Refresh")
+    menu.append(menu_items)
+    menu_items.connect("activate", refresh)
+    menu_items.show_all()
 
-  menu_items = Gtk.MenuItem(label="LED")
-  menu.append(menu_items)
-  menu_items.show_all()
-  menu_items.set_submenu(led_menu())
-  
-  if len(argv) == 3:
-    menu_items = Gtk.MenuItem(label="Switch")
+    menu_items = Gtk.MenuItem(label="Chat: -1")
     menu.append(menu_items)
     menu_items.show_all()
-    menu_items.set_submenu(switch_menu())
+    chatmix = menu_items
 
-  menu_items = Gtk.MenuItem(label="Exit")
-  menu.append(menu_items)
-  menu_items.connect("activate", quit)
-  menu_items.show_all()
-  
-  ind.set_menu(menu)
+    menu_items = Gtk.MenuItem(label="Sidetone")
+    menu.append(menu_items)
+    menu_items.show_all()
+    menu_items.set_submenu(sidetone_menu())
 
-  # if we have switchSound binary, we can try to detect current output
-  if len(argv) == 3:
-    GLib.timeout_add(60000, change_icon, None)
+    menu_items = Gtk.MenuItem(label="LED")
+    menu.append(menu_items)
+    menu_items.show_all()
+    menu_items.set_submenu(led_menu())
 
-  # update printed charge every 60 seconds
-  GLib.timeout_add(60000, change_label, None)
-  GLib.timeout_add(60000, change_chatmix, None)
+    if len(argv) == 3:
+        menu_items = Gtk.MenuItem(label="Switch")
+        menu.append(menu_items)
+        menu_items.show_all()
+        menu_items.set_submenu(switch_menu())
 
-  # refresh values right away
-  refresh(None)
+    menu_items = Gtk.MenuItem(label="Exit")
+    menu.append(menu_items)
+    menu_items.connect("activate", quit_app)
+    menu_items.show_all()
 
-  Gtk.main()
+    ind.set_menu(menu)
+
+    # if we have switchSound binary, we can try to detect current output
+    if len(argv) == 3:
+        GLib.timeout_add(60000, change_icon, None)
+
+    # update printed charge every 60 seconds
+    GLib.timeout_add(60000, change_label, None)
+    GLib.timeout_add(60000, change_chatmix, None)
+
+    # refresh values right away
+    refresh(None)
+
+    Gtk.main()
